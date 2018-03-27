d8dcd25fe6a9ff49611b79094b2cbaeb514c1ca5
sxx <425033441@qq.com>
2018-03-27 19:39:10 +0800

    asdsadg

--- app.js
+++ app.js
@@ -12,16 +12,17 @@ const app = express()
 //jquery
 app.use('/public',express.static('./public/'))
 app.use('./node_modules',express.static('./node_modules/'))
+
 //配置 body-parser 解析表单 POST 请求体
 //只有配置了该插件，就可以在请求处理函数中使用req.body 来访问请求体数据了
-
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 //parse application/json
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 
 //配置使用art-template
 app.engine('html',require('express-art-template'))
+
 app.use(router)
 
 app.listen(3000,() => console.log('服务器正在运行中······'))
--- router.js
+++ router.js
@@ -1,16 +1,14 @@
 //加载 express
 const express = require('express')
 
-
 //加载所有的处理函数模块
 const index = require('./controllers/index')
-const index = require('./controllers/topic')
+const topic = require('./controllers/topic')
 const user = require('./controllers/user')
 
 
 //1.调用 express.Router() 创建一个路由实例
 const router = express.Router() 
-
 
 //2.配置路由规则
 
@@ -24,16 +22,16 @@ router
     .post('/signin',user.signin)
     .get('/signup',user.showSignup)
     .post('/signup',user.signup)
-    .post('/signout',user.signout)
+    .get('/signout',user.signout)
 
 //话题相关
 router
     .get('./topic/create',topic.showCreate)
-    .get('./topic/create',topic.create)
+    .post('./topic/create',topic.create )
     .get('./topic/topicID',topic.show)
     .get('./topic/topicID/edit',topic.showEdit)
-    .get('./topic/topicID/edit',topic.edit)
-    .get('./topic/topicID/delete',topic.delete)
+    .post('./topic/topicID/edit',topic.edit)
+    .post('./topic/topicID/delete',topic.delete)
 
 //3.导出路由对象
 module.exports = router 
